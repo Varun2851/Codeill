11c64a83495a6825edb11cc53afbd691de0b269e
Varun Yadav <varunyadav891@gmail.com>
2023-06-19 22:22:46 +0530

    Created Schema for Post

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -1,7 +1,23 @@
-module.exports.posts = function(req, res)
-{
-    return res.render('posts',
-    {
-        title: 'Posts'
-    });
+// module.exports.posts = function(req, res)
+// {
+//     return res.render('posts',
+//     {
+//         title: 'Posts'
+//     });
+// }
+
+const Post = require('../models/post');
+
+module.exports.create = function(req,res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    })
+        .then((post) => {
+          return res.redirect('back');
+        })
+        .catch((err) => {
+          console.log('error in creating a post', err);
+        });
+      
 }
