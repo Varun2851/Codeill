11a22f06921f314615d11aacbf6caa45e3cf1d2a
Varun Yadav <varunyadav891@gmail.com>
2023-06-24 11:25:06 +0530

    added the feature of Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,50 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res){
     //return res.end('<h1>Express is up for codiel</h1>');
-    console.log(req.cookies);
+    //console.log(req.cookies);
     //res.cookie('user_id');
-    return res.render('home',{
-        title : "Home"
-    });
+
+    // Post.find({},function(err,posts){
+
+
+    //     return res.render('home',{
+    //         title : "Impressions | Home",
+    //         posts : posts
+    //     });
+
+    // });
+
+
+//   Post.find({})
+//   .then(posts => {
+//     return res.render('home', {
+//       title: "Impressions | Home",
+//       posts: posts
+//     });
+//   })
+//   .catch(err => {
+//     // Handle error appropriately
+//     console.error(err);
+//     // Send an error response
+//     return res.status(500).send('Internal Server Error');
+//   });
+
+Post.find({})
+.populate('user')
+.exec()
+.then(posts => {
+  return res.render('home', {
+    title: "Impressions | Home",
+    posts: posts
+  });
+})
+.catch(err => {
+  // Handle error appropriately
+  console.error(err);
+  // Send an error response
+  return res.status(500).send('Internal Server Error');
+});
+
+ 
 } 
 //module.export.actionName = function(req,res){}
